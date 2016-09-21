3afe7300be21e2d41446abf555dfeaf525f2e203
juseongYun <yjs2896@naver.com>
2016-09-21 13:10:16 +0900

    Revert "Sources"
    This reverts commit 29f3212833012d954f1fe720b0130b892e7ace64.

--- Server/Socket/compile.sh
+++
@@ -1,2 +0,0 @@
-gcc -o main.out main.c mobile.c alarm.c arduino.c lib.c -L /usr/lib/ -l pthread -lmysqlclient -g
-echo " gcc -o main.out main.c mobile.c alarm.c arduino.c lib.c -L /usr/lib/ -l pthread -lmysqlclient -g";
