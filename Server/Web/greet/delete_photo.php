248fe3fa78375595bf7f5804494ce12a55c784b3
juseongYun <yjs2896@naver.com>
2016-09-24 16:25:02 +0900

    change in arduino code

--- Server/Web/greet/delete_photo.php
+++ Server/Web/greet/delete_photo.php
@@ -4,10 +4,19 @@
 
    include "../lib/dbconn.php";
 
+   
+   $sql = "select file_copied from photo where num =".$_GET['num'];
+   $result = mysql_query($sql, $connect);
+   $row = mysql_fetch_array($result);
+   			
+   $upload_dir = './data/';
+   $file = $upload_dir."/".$row['file_copied'];
+   if($row['file_copied'] &&is_file($file)) unlink($file);
+   
    $sql = "delete from photo where num =".$_GET['num'];
    mysql_query($sql, $connect);
    mysql_close();
-
+   
    echo "
 	   <script>
       opener.location.reload();
